2afc5342eb1ccd79dfd1b33676bf062938a94445
Zhen Data <zhenyang.9261@gmail.com>
2019-12-14 22:41:45 -0500

    Update burger works!!

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -19,4 +19,22 @@ $(function() {
       location.reload();
     });
   });
+
+  $(".devour-btn").on("click", function(event) {
+    var id = $(this).data("id");
+
+    var newDevourState = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burger/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(function() {
+      console.log("changed devour to true");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 });
