9651c9488523f573ca7bcbf26ee153cd52bc7d5c
Zhen Data <zhenyang.9261@gmail.com>
2019-12-16 23:01:52 -0500

    Added Heroku db url config

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@ var mysql = require("mysql");
 var user_pwd = require("./db_credential");
 
 // Set up MySQL connection.
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: user_pwd.user,
-  password: user_pwd.password,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: user_pwd.user,
+    password: user_pwd.password,
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
